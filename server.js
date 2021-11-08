f51015228b04fdfcd6e09d0bbc69ce05a6cdfd25
vJanos95 <vargajanos16@gmail.com>
2021-11-08 18:14:35 +0100

    another GET request based on the ID

--- server.js
+++ server.js
@@ -1,46 +1,56 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let Item = require("./Schemas/Item");
+let Item = require('./Schemas/Item');
 
-const database = mongoose.connect("mongodb://localhost/practice-db");
+const database = mongoose.connect('mongodb://localhost/practice-db');
 
 app.listen(3000, function () {
-    console.log("Now running on port 3000!");
+    console.log('Now running on port 3000!');
 });
 
-app.post("/items/additem", function (request, response) {
+app.post('/items/additem', function (request, response) {
     let dbitem = new Item(request.body);
 
     dbitem.save(function (error, savedItem) {
         if (error) {
-            response.status(500).send("Error creating item");
+            response.status(500).send('Error creating item');
         } else {
             response.send(savedItem);
         }
     });
 });
 
-app.get("/items", function (request, response) {
+app.get('/items', function (request, response) {
     Item.find({}, function (error, items) {
         if (error) {
-            response.status(500).send("Failed to get items from the database");
+            response.status(500).send('Failed to get items from the database');
         } else {
             response.send(items);
         }
     });
 });
 
-app.delete("/items/removeitem/:itemID", function (request, response) {
+app.get('/items/:itemID', function (request, response) {
+    Item.findById({ _id: request.params.itemID }, function (error, item) {
+        if (error) {
+            response.status(500).send({ error: 'Could not find the item' });
+        } else {
+            response.send(item);
+        }
+    });
+});
+
+app.delete('/items/removeitem/:itemID', function (request, response) {
     Item.deleteOne(
         { _id: request.params.itemID },
         function (error, deletedItem) {
             if (error) {
-                response.status(500).send("Failed to delete the given item");
+                response.status(500).send('Failed to delete the given item');
             } else {
                 response.send(
                     `Item with the id of "${request.params.itemID}" was deleted from the database!`
@@ -50,7 +60,7 @@ app.delete("/items/removeitem/:itemID", function (request, response) {
     );
 });
 
-app.put("/items/modify/:itemID", function (request, response) {
+app.put('/items/modify/:itemID', function (request, response) {
     Item.findByIdAndUpdate(
         request.params.itemID,
         {
@@ -60,9 +70,9 @@ app.put("/items/modify/:itemID", function (request, response) {
         },
         function (error, items) {
             if (error) {
-                response.status(500).send("Error while modifying the item");
+                response.status(500).send('Error while modifying the item');
             } else {
-                response.send("Item updated successfully!");
+                response.send('Item updated successfully!');
             }
         }
     );
